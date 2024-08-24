4e87cd25da4f0f2a2c2582208f83204366f62f2a
fellpos <ra54399754893@acaonsfatima.org.br>
2024-08-24 20:02:18 -0300

    .

--- src/repository/filmeRepository.js
+++ src/repository/filmeRepository.js
@@ -17,16 +17,17 @@ let obj =
 export async function salvarFilme(filme) {
     let comando = `
         INSERT INTO tb_filme (nm_filme, ds_sinopse, vl_avaliacao, dt_lancamento, bt_disponivel)
-           VALUES (?, ?, ?, ?, ?)
-     
+            VALUES (?, ?, ?, ?, ?)
     `
 // query = raiozinho / executa
 // entre [] ele substitui as ?
 // retorna uma variavel
 // colocar await e async
+
     let reposta = await con.query(comando, [filme.nome, filme.sinopse, filme.avaliacao, filme.lancamento, filme.disponivel]);
-    // resposta é um vetor e a info que queremos sempre vão estar em 0
+// resposta é um vetor e a info que queremos sempre vão estar em 0
     let info = reposta[0];
+    
 // chave primaria q o banco criou 
     let idFilme = info.insertId
 
